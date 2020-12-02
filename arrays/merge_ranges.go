570c01f08fe506a0d34327b7fe12501920a01ba6
aponte411 <aponted511@gmail.com>
2020-12-02 17:25:38 -0500

    feat: finished MergeRanges

--- arrays/merge_ranges.go
+++ arrays/merge_ranges.go
@@ -2,18 +2,27 @@ package arrays
 
 import "sort"
 
-// TODO: need to finish this
+// O(N) time, O(N) space
 func MergeRanges(nums [][]int) [][]int {
+    // Sort by first element, i.e. sort by start of meetings
+    // to get natural order of meetings.
 	sort.Slice(nums, func(i, j int) bool {
-		return nums[i] < nums[j]
+		return nums[i][0] < nums[j][0]
 	})
-	merged := make([]int, nums[len(nums)-1])
+	merged := make([][]int, 0)
+	merged = append(merged, nums[0])
 	for _, num := range nums {
-		start, end := num
+		start, end := num[0], num[1]
 		next_ending := merged[len(merged)-1]
+        // If the last meetings end time is
+        // greater than the start time of this
+        // current meeting, merge.
 		if next_ending[1] >= start {
+            // assign to last element in merged to
+            // always get the next ending.
 			merged[len(merged)-1] = []int{
-				next_ending[0], max(next_ending[1], end),
+				next_ending[0],
+				max(next_ending[1], end),
 			}
 		} else {
 			merged = append(merged, []int{start, end})
--- arrays/merge_ranges_test.go
+++ arrays/merge_ranges_test.go
@@ -1,11 +1,29 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestMergeRanges(t *testing.T) {
-	ans1 := MergeRanges([]int{1, 3}, []int{2, 4})
-	exp1 := []int{[]int{1, 4}}
-	ans2 := MergeRanges([]int{5, 6}, []int{6, 8})
-	exp2 := []int{[]int{5, 8}}
+	list1 := [][]int{
+		{1, 3},
+		{2, 4},
+	}
+	ans1 := MergeRanges(list1)
+	exp1 := [][]int{{1, 4}}
+	if !reflect.DeepEqual(ans1, exp1) {
+		t.Errorf("Expected %v got %v", exp1, ans1)
+	}
+
+	list2 := [][]int{
+		{5, 6},
+		{6, 8},
+	}
+	ans2 := MergeRanges(list2)
+	exp2 := [][]int{{5, 8}}
+	if !reflect.DeepEqual(ans2, exp2) {
+		t.Errorf("Expected %v got %v", exp2, ans2)
+	}
 
 }
