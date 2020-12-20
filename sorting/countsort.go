f5d25cb3f1b00e14181d6baa50587432a90d9b58
David Aponte <davidaponte@system76-pc.localdomain>
2020-12-20 15:44:33 -0500

    feat: add countsort

--- sorting/countsort.go
+++ sorting/countsort.go
@@ -1,5 +1,22 @@
 package sorting
 
 func CountSort(nums []int, max int) []int {
-
+	// Build a slice of counts where the indicies are the
+	// numbers and the values are the counts.
+	counts := make([]int, max+1) // max(nums)
+	results := make([]int, 0)
+	for _, num := range nums {
+		counts[num] += 1
+	}
+	// Walk through the counts and add the number to the results
+	// as many times as it was present in the counts
+	i := len(counts) - 1
+	for i >= 0 {
+		count := counts[i]
+		for j := 0; j < count; j++ {
+			results = append(results, i)
+		}
+		i -= 1
+	}
+	return results
 }
--- sorting/countsort_test.go
+++ sorting/countsort_test.go
@@ -2,6 +2,9 @@ package sorting
 
 import "testing"
 
-func TestCountSort(t *testing.T){
-    res := CountSort([]int{10, 50, 25, 77, 88, 18}, 100)
+func TestCountSort(t *testing.T) {
+	result := CountSort([]int{37, 89, 41, 65, 91, 53}, 100)
+	if result[0] != 91 {
+		t.Errorf("Expected %v, got %v", 91, result[0])
+	}
 }
