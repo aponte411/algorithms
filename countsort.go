85d1c594652d38aea346428564c3086b5f4227d0
David Aponte <davidaponte@system76-pc.localdomain>
2020-12-20 11:18:13 -0500

    wip: adding count sort

---
+++ countsort.go
@@ -0,0 +1,5 @@
+package sorting
+
+func CountSort(nums []int, max int) []int {
+
+}
---
+++ countsort_test.go
@@ -0,0 +1,7 @@
+package sorting
+
+import "testing"
+
+func TestCountSort(t *testing.T){
+    res := CountSort([]int{10, 50, 25, 77, 88, 18}, 100)
+}
