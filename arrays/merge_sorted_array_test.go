60a864f0c8ac60a4bf86d3f4a14de5a6ee952198
David Aponte <davidaponte@system76-pc.localdomain>
2020-12-20 21:16:46 -0500

    fix: fix test

--- arrays/merge_sorted_array_test.go
+++ arrays/merge_sorted_array_test.go
@@ -1,14 +1,18 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestMergeArray(t *testing.T) {
 	nums1 := []int{1, 2, 3, 0, 0, 0}
 	nums2 := []int{2, 5, 6}
+	exp := []int{1, 2, 2, 3, 5, 6}
 	m := 3
 	n := 3
 	MergeArrays(nums1, m, nums2, n)
-	if nums1[len(nums1)-1] != 6 {
-		t.Errorf("Expected 6, got %v", nums1[len(nums1)])
+	if !reflect.DeepEqual(nums1, exp) {
+		t.Errorf("Expected %v, got %v", nums1, exp)
 	}
 }
