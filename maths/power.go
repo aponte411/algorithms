39c794d0d9656ab6adf9991333d0be270e74762a
aponte411 <aponted511@gmail.com>
2020-12-21 14:19:18 -0500

    chore: docs

--- maths/power.go
+++ maths/power.go
@@ -15,7 +15,7 @@ func Power(x float64, n int) float64 {
 }
 
 // O(log n) time, and O(1) space
-// PingalaPower Aglorithm
+// PingalaPower Aglorithm, goes back to 2nd century BCE
 func PingalaPower(x float64, n int) float64 {
 	N := n
 	if N < 0 {
@@ -25,12 +25,14 @@ func PingalaPower(x float64, n int) float64 {
 	return power(x, N)
 }
 
+// T(n) <= T(n/2) + 2 -> O(log n)
 func power(x float64, n int) float64 {
 	if n == 0 {
 		return 1.0
 	}
+    // Reducing space by half
 	half := power(x, n/2)
-	if n%2 == 0 {
+	if n % 2 == 0 {
 		return half * half
 	} else {
 		return half * half * x
