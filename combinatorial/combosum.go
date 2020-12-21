ebf98f81682d993ae1c1d906bcf2efcbbd6f3268
David Aponte <davidaponte@system76-pc.localdomain>
2020-12-20 21:05:23 -0500

    chore: format

--- combinatorial/combosum.go
+++ combinatorial/combosum.go
@@ -9,21 +9,21 @@ func ComboSum(nums []int, target int) [][]int {
 // bottom up helper that finds all combos for a given candidate and stops
 // when it reaches the target sum
 func helper(nums []int, combos *[][]int, combo []int, curr, target, index int) {
-    // If we've reached the target, add the current combination to the results
+	// If we've reached the target, add the current combination to the results
 	if curr == target {
 		*combos = append(*combos, combo)
 		return
 	}
-    // If we pass it, return
+	// If we pass it, return
 	if curr > target {
 		return
 	}
 	for i := index; i < len(nums); i++ {
-        // Add current candidate then combo everything from current combo
+		// Add current candidate then combo everything from current combo
 		comboCopy := make([]int, 0)
 		comboCopy = append(comboCopy, nums[i])
 		comboCopy = append(comboCopy, combo...)
-        // Increment current sum by candidate
+		// Increment current sum by candidate
 		helper(nums, combos, comboCopy, curr+nums[i], target, i)
 	}
 }
