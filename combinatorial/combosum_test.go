4a6365328c686bec85f92add514b59ab47f89535
David Aponte <davidaponte@system76-pc.localdomain>
2020-12-20 21:18:18 -0500

    fix: fix combosum test as well

--- combinatorial/combosum_test.go
+++ combinatorial/combosum_test.go
@@ -7,12 +7,12 @@ import (
 
 func TestComboSum(t *testing.T) {
 	exp := [][]int{
-		{2, 2, 3},
+		{3, 2, 2},
 		{7},
 	}
 	inputs := []int{2, 3, 6, 7}
 	res := ComboSum(inputs, 7)
-	if reflect.DeepEqual(exp, res) {
+	if !reflect.DeepEqual(exp, res) {
 		t.Errorf("Expected %v, got %v", exp, res)
 	}
 }
