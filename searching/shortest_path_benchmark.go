ad61bd371157837761f4358b64656210701ee734
aponte411 <aponted511@gmail.com>
2020-12-29 23:37:14 -0500

    fix: add benchmark, fix tests

---
+++ searching/shortest_path_benchmark.go
@@ -0,0 +1,26 @@
+package searching
+
+import (
+    "testing"
+    "fmt"
+)
+
+func BenchmarkShortestPath(b *testing.B){
+    graph := map[string][]string{
+		"Min":     []string{"William", "Jayden", "Omar"},
+		"William": []string{"Min", "Noam"},
+		"Jayden":  []string{"Min", "Amelia", "Ren", "Noam"},
+		"Ren":     []string{"Jayden", "Omar"},
+		"Amelia":  []string{"Jayden", "Adam", "Miguel"},
+		"Adam":    []string{"Amelia", "Sofia", "Miguel", "Lucas"},
+		"Miguel":  []string{"Amelia", "Adam", "Liam", "Nathan"},
+		"Noam":    []string{"Nathan", "Jayden", "William"},
+		"Omar":    []string{"Ren", "Min", "Scott"},
+	}
+	src := "Jayden"
+	dst := "Adam"
+    for n := 0; n < b.N; n++{
+        res := FindAShortestPath(graph, src, dst)
+        fmt.Println(res)
+    }
+}
